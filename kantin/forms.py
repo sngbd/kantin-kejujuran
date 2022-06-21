1e307c243ba6a25feef31f9bf4caf5d22f21c1ea
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-06-21 11:01:53 +0700

    stylize forms

--- kantin/forms.py
+++ kantin/forms.py
@@ -2,14 +2,34 @@ from django import forms
 
 
 class CreateForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    description = forms.CharField(widget=forms.Textarea)
-    price = forms.IntegerField(min_value=0, label="Price (IDR)")
-    image = forms.CharField(max_length=500, required=True, label="Image (URL)")
+    name = forms.CharField(label='', max_length=100, widget=forms.TextInput(
+       attrs={
+           'class': 'form-control',
+           'placeholder': 'Name',
+       }))
+    description = forms.CharField(label='', widget=forms.Textarea(
+       attrs={
+           'class': 'form-control',
+           'placeholder': 'Description',
+       }))
+    price = forms.IntegerField(min_value = 0, label='', widget=forms.NumberInput(
+       attrs={
+           'class': 'form-control',
+           'placeholder': 'Price (IDR)',
+       }))
+    image = forms.CharField(max_length=1000, required=True, label='', widget=forms.TextInput(
+        attrs={
+           'class': 'form-control',
+           'placeholder': 'Image (URL)',
+        }))
 
 
 class BalanceBox(forms.Form):
-    balance = forms.IntegerField(min_value=0, label="Insert balance")
+    balance = forms.IntegerField(min_value = 0, label='', widget=forms.NumberInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Insert balance',
+        }))
     
 
 choices = [
@@ -20,4 +40,8 @@ choices = [
 
 
 class SortChoice(forms.Form):
-    sort = forms.ChoiceField(choices=choices, widget=forms.Select(attrs={"onChange":'this.form.submit()'}))
+    sort = forms.ChoiceField(label='', choices=choices, widget=forms.Select(
+        attrs={
+            'onChange':'this.form.submit()',
+            'class': 'form-control',
+        }))
